67a7a49bc0d3325840c8b60d8d9efd76e4a0949f
ZhidanRn <zhidanir@gmail.com>
2024-04-17 14:32:50 +0700

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ProductList from './components/ProductList'
+import AddProduct from './components/AddProduct'
+import EditProduct from './components/EditProduct'
+
+function App() {
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<ProductList />} />
+          <Route path='/add' element={<AddProduct />} />
+          <Route path='/edit/:id' element={<EditProduct />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
